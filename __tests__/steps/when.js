e5e36b0c61408165b8ea8e040c4f69f970557910
s4nt14go <santiagopergamo@gmail.com>
2020-11-22 23:43:53 -0300

    Add acceptance tests for reply mutation

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -81,6 +81,41 @@ fragment retweetFields on Retweet {
     ... on Tweet {
       ... tweetFields
     }
+    ... on Reply {
+      ... replyFields
+    }
+  }
+}
+`
+
+const replyFragment = `
+fragment replyFields on Reply {
+  id
+  profile {
+    ... iProfileFields
+  }
+  createdAt
+  text
+  replies
+  likes
+  retweets
+  retweeted
+  liked
+  inReplyToTweet {
+    id
+    profile {
+      ... iProfileFields
+    }
+    createdAt
+    ... on Tweet {
+      replies
+    }
+    ... on Reply {
+      replies
+    }
+  }
+  inReplyToUsers {
+    ... iProfileFields
   }
 }
 `
@@ -93,6 +128,9 @@ fragment iTweetFields on ITweet {
   ... on Retweet {
     ... retweetFields
   }
+  ... on Reply {
+    ... replyFields
+  }
 }
 `
 
@@ -101,6 +139,7 @@ registerFragment('otherProfileFields', otherProfileFragment)
 registerFragment('iProfileFields', iProfileFragment)
 registerFragment('tweetFields', tweetFragment)
 registerFragment('retweetFields', retweetFragment)
+registerFragment('replyFields', replyFragment)
 registerFragment('iTweetFields', iTweetFragment)
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
@@ -435,7 +474,9 @@ const a_user_calls_getLikes = async (user, userId, limit, nextToken) => {
 
 const a_user_calls_retweet = async (user, tweetId) => {
   const retweet = `mutation retweet($tweetId: ID!) {
-    retweet(tweetId: $tweetId)
+    retweet(tweetId: $tweetId) {
+      ... retweetFields
+    }
   }`
   const variables = {
     tweetId
@@ -465,6 +506,25 @@ const a_user_calls_unretweet = async (user, tweetId) => {
   return result
 }
 
+const a_user_calls_reply = async (user, tweetId, text) => {
+  const reply = `mutation reply($tweetId: ID!, $text: String!) {
+    reply(tweetId: $tweetId, text: $text) {
+      ... replyFields
+    }
+  }`
+  const variables = {
+    tweetId,
+    text
+  }
+
+  const data = await GraphQL(process.env.API_URL, reply, variables, user.accessToken)
+  const result = data.reply
+
+  console.log(`[${user.username}] - replied to tweet [${tweetId}]`)
+
+  return result
+}
+
 module.exports = {
   we_invoke_confirmUserSignup,
   we_invoke_getImageUploadUrl,
@@ -485,4 +545,5 @@ module.exports = {
   a_user_calls_getLikes,
   a_user_calls_retweet,
   a_user_calls_unretweet,
+  a_user_calls_reply
 }
